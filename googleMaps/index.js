84dd1b7398f557980d4a28f322c8a2041fb5c776
Catherine <catherine@serpapi.com>
2025-09-05 14:20:42 -0700

    Initial commit

---
+++ googleMaps/index.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+import { getJson } from "serpapi";
+import fs, { write } from "fs";
+import path from "path"
+
+dotenv.config();
+const apiKey = process.env.API_KEY;
+const writeToCsv = (data) => {
+    const csvContent = data.map(row => row.join(",")).join("\n");
+    const filePath = path.join(process.cwd(), "output.csv");
+    fs.writeFile(filePath, csvContent, "utf8", (err) => {
+        if (err) {
+            console.error("Error writing CSV file:", err);
+        } else {
+            console.log("CSV file has been saved to", filePath);
+        }
+    });
+}
+getJson({
+  engine: "google_maps",
+  q: "Coffee",
+  ll: "@40.76173745837114,-73.9774600487914,14z",
+  api_key: apiKey,
+  type: "search"
+}, (json) => {
+  const data = [];
+  data.push(["Name", "Phone", "Rating", "Reviews", "Address"]);
+
+  json.local_results.forEach(({title, phone, rating, reviews, address}) => data.push([title, phone, rating, reviews, address]));
+
+  writeToCsv(data)
+});
---
+++ video-1/scrapeData.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+import { getJson } from "serpapi";
+
+dotenv.config();
+
+const apiKey = process.env.API_KEY;
+
+//Try catch?
+const writeToCsv = (data) => {
+    const csvContent = data.map(row => row.join(",")).join("\n");
+    const filePath = path.join(process.cwd(), "output.csv");
+    fs.writeFile(filePath, csvContent, "utf8", (err) => {
+        if (err) {
+            console.error("Error writing CSV file:", err);
+        } else {
+            console.log("CSV file has been saved to", filePath);
+        }
+    });
+}
+
+try {
+    const json = await getJson({
+        api_key: apiKey,
+        engine: "google_maps",
+        type: "search",
+        q: "Coffee",
+        ll: "@40.76173745837114,-73.9774600487914,14z"
+    });
+
+    if (!json.local_results || json.local_results.length === 0) {
+        throw new Error("No results found.");
+    }
+    const data = [
+        ["Name", "Phone Number", "Rating", "Reviews", "Address"],
+        ...json.local_results.map(result => [
+            `"${result.title}"`,
+            result.phone,
+            result.rating,
+            result.reviews,
+            `"${result.address}"`,
+        ])
+    ];
+
+    writeToCsv(data);
+} catch (error) {
+    console.error("Error fetching data:", error);
+}
