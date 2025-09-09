a3f954bc3059b5bd8d649a57bc92c23b90a52a3a
Catherine <catherine@serpapi.com>
2025-09-09 12:43:57 -0700

    Finished Google Light Search API

---
+++ keywordRanking/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+import { getJson } from "serpapi";
+
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+const keywords = [
+    "coffee",
+    "iced coffee",
+    "oat latte",
+    "best coffee",
+    "americano",
+    "latte",
+    "matcha",
+    "pumpkin spice latte",
+    "oat milk latte",
+];
+
+
+//   the domain to check the rankings of
+// const domain = "https://serpapi.com";
+
+const data = await getJson({
+    api_key: apiKey,
+    engine: "google_light",
+    google_domain: "google.com",
+    q: keywords[0],//required 
+    gl: "us"
+})
+
+console.log(data)
+const responses = keywords.map(async (keyword) =>
+    getJson({
+        api_key: apiKey,
+        engine: "google_light",
+        google_domain: "google.com",
+        q: keyword,//required 
+        gl: "us"
+    })
+);
+
+const json = await Promise.all(responses);
+
