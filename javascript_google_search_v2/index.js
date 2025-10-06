b8d618b4dd521a3506e71c91fbf6f713d9a40790
Catherine <catherine@serpapi.com>
2025-10-06 10:02:14 -0700

    Finished google search tutorial code

--- javascript_google_search_v2/index.js
+++ javascript_google_search_v2/index.js
@@ -6,18 +6,18 @@ const apiKey = process.env.API_KEY;
 
 getJson({
     api_key: apiKey,
-    q: "500g to kg",
+    q: "matcha",
     engine: "google",
     gl: "ca",
     location: "Vancouver, British Columbia, Canada",
     start: 30
 }, (json) => {
-    // const data = json.organic_results;
-    // data.forEach((result, index) => {
-    //     console.log(`${index + 1}. ${result.title} - ${result.link}`);
-    //     console.log("Snippet:", result.snippet);
-    //     console.log("-----");
-    //     console.log("")
-    // });
-    console.log(json)
+    const data = json.organic_results;
+    data.forEach((result, index) => {
+        console.log(`${index + 1}. ${result.title} - ${result.link}`);
+        console.log("Snippet:", result.snippet);
+        console.log("-----");
+        console.log("")
+    });
+   
 })
