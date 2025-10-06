942f7c76e491228eb7d72628da85543f4ad4a6e1
Catherine <catherine@serpapi.com>
2025-10-06 10:00:28 -0700

    Finished tutorial code

---
+++ javascript_google_search_v2/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import { getJson } from "serpapi";
+import fs from "fs"
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+getJson({
+    api_key: apiKey,
+    q: "500g to kg",
+    engine: "google",
+    gl: "ca",
+    location: "Vancouver, British Columbia, Canada",
+    start: 30
+}, (json) => {
+    // const data = json.organic_results;
+    // data.forEach((result, index) => {
+    //     console.log(`${index + 1}. ${result.title} - ${result.link}`);
+    //     console.log("Snippet:", result.snippet);
+    //     console.log("-----");
+    //     console.log("")
+    // });
+    console.log(json)
+})
