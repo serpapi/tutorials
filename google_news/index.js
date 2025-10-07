5c04154a6a6d1027397c67f3f18eda3b62761db8
Catherine <catherine@serpapi.com>
2025-10-07 10:20:15 -0700

    Google news complete

---
+++ google_news/index.js
@@ -0,0 +1,52 @@
+import dotenv from "dotenv";
+import { getJson } from "serpapi";
+import fs from "fs"
+dotenv.config();
+const apiKey = process.env.API_KEY;
+const categories = []
+const googleNews = await getJson({
+    api_key: apiKey,
+    engine: "google_news",
+    gl: "ca",
+    hl: 'en'
+});
+
+googleNews.menu_links.forEach(element => {
+    categories.push({ title: element.title, topic_token: element.topic_token });
+});
+
+const categories2 = [{
+    title: 'Sports',
+    topic_token: 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKRFFTZ0FQAQ'
+},
+{
+    title: 'Science',
+    topic_token: 'CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp0Y1RjU0JXVnVMVWRDR2dKRFFTZ0FQAQ'
+},
+{
+    title: 'Health',
+    topic_token: 'CAAqJQgKIh9DQkFTRVFvSUwyMHZNR3QwTlRFU0JXVnVMVWRDS0FBUAE'
+}]
+const newsByCategory = categories2.map(async (category) => {
+    return getJson({
+        api_key: apiKey,
+        engine: "google_news",
+        topic_token: category.topic_token,
+        gl: "us",
+        hl: 'en'
+    });
+})
+
+const news = await Promise.all(newsByCategory);
+const jsonString = JSON.stringify(news, null, 2);
+
+news.forEach((item) => {
+    console.log("CATEGORY: " + item.title)
+    const topResults = item.news_results.slice(0, 5);
+    topResults.forEach(res => {
+        console.log(res.highlight? res.highlight.title : res.title)
+    })
+    console.log("")
+    console.log("---------------------")
+    console.log("")
+})
