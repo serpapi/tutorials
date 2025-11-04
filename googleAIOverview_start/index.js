e3a8db077fd05dad58d9ae74b85df9ebd9b38896
Catherine <catherine@serpapi.com>
2025-11-04 13:37:46 -0800

    Finished google AI Overview tutorial

--- googleAIOverview_start/index.js
+++ googleAIOverview_start/index.js
@@ -1,32 +1,34 @@
 import dotenv from "dotenv";
 import { getJson } from "serpapi";
 import { colorize } from "json-colorizer";
+
 dotenv.config();
 const apiKey = process.env.API_KEY;
 
 getJson({
     api_key: apiKey,
-    engine: "google",
-    gl: 'fj',
-    q: "how to brew coffee",
-
+    q: "how do you brew coffee",
+    no_cache: true,
+    gl: "fj",
 }, (searchResult) => {
     if (searchResult?.ai_overview === undefined) {
         console.log("No AI Overview");
         return;
     }
-    console.log("*** AI OVERVIEW ***")
+    console.log("*** AI OVERVIEW ***");
+    console.log(colorize(searchResult.ai_overview));
 
-    const pageToken = searchResult.ai_overview?.page_token
+    const pageToken = searchResult.ai_overview?.page_token;
     if (pageToken === undefined) {
         console.log(colorize(searchResult.ai_overview));
     } else {
+        //Need to make another API call
         console.log("*** API REQUEST TO AI OVERVIEW API ***")
+
         getJson({
             api_key: apiKey,
             engine: "google_ai_overview",
             page_token: pageToken
-        }, (aiOverview) => console.log(colorize(aiOverview)))
-
+        }, (aiOverview) => console.log(colorize(aiOverview.ai_overview)))
     }
 })
