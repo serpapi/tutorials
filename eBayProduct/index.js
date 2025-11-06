589708f4b43a26c27f462c6607776bd1bbbd4640
Catherine <catherine@serpapi.com>
2025-11-06 14:19:09 -0800

    starting ebay product api tutorial

---
+++ eBayProduct/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import { getJson } from "serpapi";
+dotenv.config();
+const apiKey = process.env.API_KEY;
+
+
+try {
+    const data = await getJson({
+        api_key: apiKey,
+        engine: "ebay_product",
+        product_id: "375538468672"
+    })
+
+    console.log("Product data fetched successfully", data)
+} catch (error) {
+    console.error("Error fetching product data: ", error)
+}
