c2738be482732145caabe6d13eb682a4e4de2821
Catherine <catherine@serpapi.com>
2026-05-27 09:00:24 -0700

    clean up console logs

--- javascript_video_tutorials/apple_app_store_api/index.js
+++ javascript_video_tutorials/apple_app_store_api/index.js
@@ -10,39 +10,36 @@ const client = new OpenAI({ apiKey: openAiKey });
 //SerpApi setup
 const serpApiKey = process.env.SERP_API_KEY;
 
-let appReviews;
+let reviews;
 try {
-
     const app = await getJson({
         api_key: serpApiKey,
         engine: "apple_app_store",
         term: "instagram",
-        num: "5"
     });
 
-    console.log("App", app)
     const appId = app?.organic_results[0]?.id;
-    appReviews = await getJson({
+
+    reviews = await getJson({
         api_key: serpApiKey,
         engine: "apple_reviews",
-        product_id: appId
+        product_id: appId,
     });
+
 } catch (err) {
-    console.error("Failed to fetch reviews from SerpApi:", err);
+    onsole.error("Failed to fetch reviews from SerpApi:", err);
     process.exit(1);
 }
 
-const textReviews = appReviews?.reviews.map(review => review.text)
+const textReviews = reviews?.reviews.map(review => review.text);
 
-let response;
 try {
-    response = await client.responses.create({
+    const summary = await client.responses.create({
         model: "gpt-5.4",
         input: `Given the list of comma-separated reviews for a mobile app, give me a summary of the main criticisms: ${textReviews.join(", ")}`,
-    });
+    })
+    console.log(summary?.output_text);
 } catch (err) {
     console.error("Failed to get response from OpenAI:", err.message);
     process.exit(1);
 }
-
-console.log(response.output_text);
