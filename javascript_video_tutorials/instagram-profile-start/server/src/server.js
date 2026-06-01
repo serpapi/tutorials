9b1fe35be00d3193ee5f8c243c852b3808b11954
Catherine <catherine@serpapi.com>
2026-06-01 10:53:54 -0700

    Initialize start of project

---
+++ javascript_video_tutorials/instagram-profile-start/server/src/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
